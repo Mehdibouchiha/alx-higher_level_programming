1bcd55e1c0b3a6c6d0840b1d26d5434690c3ba5f
root <bouchihamehdi409@gmail.com>
2023-11-28 15:48:04 -0800

    7-main.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-charend = ", "
-for i in range(0, 100):
-    if i < 10:
-        print("{}{}".format(0, i), end=charend)
-    else:
-        if i == 99:
-            charend = "\n"
-        print(i, end=charend)
+def islower(c):
+    if ord(c) > 96 and ord(c) < 123:
+        return True
+    return False
