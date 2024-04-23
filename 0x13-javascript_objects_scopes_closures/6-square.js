acec0b0eaf1d6f4770fcb92734e5e173750a19ff
MEHDIBOUCHIHA <bouchihamehdi409@gmail.com>
2024-04-23 22:40:03 +0100

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
-const SquareP = require('./5-square');
+const OldSquare = require('./5-square');
 
-class Square extends SquareP {
-  charPrint (c) {
+class Square extends OldSquare {
+  constructor(size) {
+    super(size);
+  }
+
+  charPrint(c) {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
-      }
-      console.log(s);
+
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
